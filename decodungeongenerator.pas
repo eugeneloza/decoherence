c907a284af50f3fbf8ab0378c5dd1926f7b17950
eugeneloza <Loza@i.ua>
2017-03-24 22:30:43 +0200

    paste Mazer code elements plus some fixes

--- decodungeongenerator.pas
+++ decodungeongenerator.pas
@@ -23,13 +23,18 @@ interface
 
 uses Classes, CastleRandom,
   decodungeontiles;
-  
+
+type
+  {maybe I'll change it later}
+  TTileType = word;
+  TIntCoordinate = integer;
+
 type
   {this is a basic "add a tile" generator step. We can build a map by followig these
    we can also use preset generatoe steps to make some tiles already available at map}
   TGeneratorStep = record
-    tile: integer;
-    x,y,z: integer;
+    tile: TTileType;
+    x,y,z: TIntCoordinate;
 end;
 
 type GeneratorStepsArray = array of TGeneratorStep;
@@ -53,16 +58,21 @@ type
     maxsteps: integer;
     {current (the last used) step of the generator}
     currentStep: integer;
-    
-    {if currentstep+1>maxsteps then setlength(..., maxsteps+100)}
-    
-    {checks if the selected tile is compatible to the already-generated map
-      usually should not be added manually, but is used inline in "AddTile"}
-    {function CheckTile(tile, x,y,z: integer): boolean; inline;}
-    {checks tile compatibility to the map and adds it if its possible}
-    {procedure AddTile(tile, x,y,z: integer)}
+    procedure ResizeSteps; {$IFDEF SUPPORTS_INLINE}inline;{$ENDIF}
+
+    {checks if the selected 1x1x1 element is compatible to the already-generated map
+     usually should not be added manually, but is used inline in "AddTile"
+     Returns false if tile mismatches Map
+     Returns True if tile matches Map and can be placed }
+    function CheckCompatibility(tile: BasicTile; x,y,z: TIntCoordinate): boolean; {$IFDEF SUPPORTS_INLINE}inline;{$ENDIF}
+    {checks tile compatibility to the map and adds it if its possible
+     returns true if tile is put successfully
+     and false if the tile cannot be placed at these coordinates  }
+    function AddTile(tile: TTileType; x,y,z: TIntCoordinate): boolean;
     {When we're sure what we are doing, we're not making any checks, just add the tile as fast as it is possible}
-    {procedure AddTileUnsafe(tile, x,y,z: integer)}
+    procedure AddTileUnsafe(tile: TTileType; x,y,z: TIntCoordinate); {$IFDEF SUPPORTS_INLINE}inline;{$ENDIF}
+    {overloaded version that accepts a TGeneratorStep;}
+     procedure AddTileUnsafe(step: TGeneratorStep);
   public
 
     {map parameters}
@@ -78,7 +88,8 @@ type
     procedure InitSeed(newseed: longword = 0);
     
     property isWorking: boolean read fisWorking;
-    { the main procedure to generate a dungeon, may be launched not-in-a-thread }
+    { the main procedure to generate a dungeon,
+      may be launched in main thread (for testing or other purposes) }
     procedure Generate;
 
     constructor Create;// override;
@@ -89,7 +100,7 @@ type
     
     //save temp state to file
   protected
-    { here we launch Generate in a Thread }
+    { here we simply launch "Generate" in a Thread }
       procedure Execute; override;
 end;
 
@@ -104,6 +115,8 @@ begin
   Generate;
 end;
 
+{========================= GENERATION ALGORITHM ==============================}
+
 {-----------------------------------------------------------------------------}
 
 procedure DDungeonGenerator.Generate;
@@ -117,11 +130,20 @@ begin
   mapx := 10;
   mapy := 10;
   mapz := 3;
-  
+
+  maxSteps := 0;
+  setlength(Gen, maxSteps);
+
   {initialize map size}
   Map.SetSize(mapx,mapy,mapz);
   Map.EmptyMap;
-  
+
+  CurrentStep := 0;
+
+  //load pregenerated tiles
+  minSteps := CurrentStep;
+
+
   //add prgenerated or undo tiles
   
   // main cycle
@@ -146,6 +168,89 @@ end;
 
 {-----------------------------------------------------------------------------}
 
+procedure DDungeonGenerator.ResizeSteps; {$IFDEF SUPPORTS_INLINE}inline;{$ENDIF}
+begin
+  if currentstep>maxsteps then begin
+    inc(maxsteps,100);
+    setlength(Gen, maxsteps+100);
+  end
+end;
+
+function DDungeonGenerator.AddTile(tile: TTileType; x,y,z: TIntCoordinate): boolean;
+var {jx,jy,jz,jj:integer;}
+    TileCanBePlaced:boolean;
+begin
+  TileCanBePlaced:=true;
+{ with tiles[InTileType] do begin
+  //check all tiles against map area they are placed to
+  for jx:=1 to tilesizex do
+   for jy:=1 to tilesizey do
+    for jz:=1 to tilesizez do TileCanBePlaced:=TileCanBePlaced and CheckTileCompatible(TileMap[jx,jy,jz],x+jx-1,y+jy-1,z+jz-1);}
+
+  if TileCanBePlaced then begin
+    // if tile can be placed - then place it
+    AddTileUnsafe(tile,x,y,z);
+    {add current step to GEN}
+    inc(currentStep);
+    ResizeSteps;
+    Gen[CurrentStep].tile := tile;
+    Gen[CurrentStep].x := x;
+    Gen[CurrentStep].y := y;
+    Gen[CurrentStep].z := z;
+
+    Result := true;
+  end else
+    Result := false;
+
+
+end;
+
+procedure DDungeonGenerator.AddTileUnsafe(tile: TTileType; x,y,z: TIntCoordinate); {$IFDEF SUPPORTS_INLINE}inline;{$ENDIF}
+{var jx,jy,jz,jj:integer; }
+begin
+{  for jx:=1 to tilesizex do
+   for jy:=1 to tilesizey do
+    for jz:=1 to tilesizez do begin
+      if TileMap[jx,jy,jz].base<>tile_na then Map^[x+jx-1,y+jy-1,z+jz-1].base:=TileMap[jx,jy,jz].base;
+      for jj:=0 to maxangles do
+        if TileMap[jx,jy,jz].faces[jj]<>tile_na then Map^[x+jx-1,y+jy-1,z+jz-1].faces[jj]:=TileMap[jx,jy,jz].faces[jj];
+      for jj:=1 to 2 do
+        if TileMap[jx,jy,jz].floor[jj]<>tile_na then Map^[x+jx-1,y+jy-1,z+jz-1].floor[jj]:=TileMap[jx,jy,jz].floor[jj];
+    end;}
+end;
+
+procedure DDungeonGenerator.AddTileUnsafe(step: TGeneratorStep);
+begin
+  AddTileUnsafe(step.tile,step.x,step.y,step.z);
+end;
+
+function DDungeonGenerator.CheckCompatibility(tile: BasicTile; x,y,z: TIntCoordinate): boolean; {$IFDEF SUPPORTS_INLINE}inline;{$ENDIF}
+{var TmpTile:Basic_Tile_Type;
+    i:integer;   }
+begin
+  Result := true;
+  {if InTile.base<>tile_na then begin
+    TmpTile:=getMap(cx,cy,cz);
+    if TmpTile.base<>tile_na then checkTileCompatible:=false else begin
+      //check current tile if it has some pre-determination
+      for i:=0 to maxangles do if (TmpTile.faces[i]<>InTile.faces[i]) and (TmpTile.faces[i]<>face_na) then CheckTileCompatible:=false;
+      for i:=1 to 2 do         if (TmpTile.floor[i]<>InTile.floor[i]) and (TmpTile.floor[i]<>floor_na) then CheckTileCompatible:=false;
+      if checkTileCompatible then begin
+        //if current tile ok, then check adjacent tiles
+        for i:=0 to maxangles do begin
+          TmpTile:=getMap(cx+a_dx(i),cy+a_dy(i),cz);
+          if TmpTile.base<>tile_inacceptible then  //might be optimized at getMap level / todo
+            if (TmpTile.faces[inverseAngle(i)]<>InTile.faces[i]) and (TmpTile.faces[inverseAngle(i)]<>face_na) then CheckTileCompatible:=false;
+        end;
+      end;
+    end;
+  end;}
+end;
+
+
+{========================= OTHER ROUTINES ===================================}
+
+
 procedure DDungeonGenerator.InitSeed(newseed: longword = 0);
 begin
   if newseed=0 then fseed := GetRandomSeed else fseed := newseed;
@@ -157,9 +262,9 @@ begin
   inherited;
   fisWorking := false;
   Map := DMap.create;
-  {$warning check non-initialized version}
-  RNDM := TCastleRandom.Create;
-  // InitSeed(GetRandomSeed); // maybe this is not optimal as operation takes some time and not required when manually initializing the seed;
+  {we create an non-initialized random (i.e. initialized by a stupid constant integer)
+  Just to make sure we don't waste any time on it now}
+  RNDM := TCastleRandom.Create(1);
 end;
 
 {-----------------------------------------------------------------------------}
