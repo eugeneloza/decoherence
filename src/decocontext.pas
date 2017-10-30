3bb5edd6293bda46a54c844e0e5e4f9ed0ec5b50
eugeneloza <Loza@i.ua>
2017-10-30 21:04:28 +0200

    DecoContext preamble

--- src/decocontext.pas
+++ src/decocontext.pas
@@ -1,3 +1,31 @@
+{Copyright (C) 2012-2017 Yevhen Loza
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.}
+
+{---------------------------------------------------------------------------}
+
+{ Context is a tool to pick a phrase/event
+  from a list based on a set of conditions
+  Each condition is a DContextElement
+  The set of conditions is DContext
+  The result is determined as "convolution" of 2 DContext instances
+  
+  Amont the tasks there are: selecting a phrase variant based on character's
+  parameters, such as gender, character, mood, health, availability of voice acting etc.
+  
+  At this point the unit is temporary, because it will be required by
+  scenario processor later in the game development }
 unit DecoContext;
 
 {$mode objfpc}{$H+}
