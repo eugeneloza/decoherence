f2baa10fa2297e0b939725e0d4bdc034fce67de3
eugeneloza <Loza@i.ua>
2017-12-22 16:10:50 +0200

    Clean purge scripts (Linux)

--- purge.sh
+++ purge.sh
@@ -3,17 +3,13 @@
 rm *.o
 rm *.ppu
 rm *.so
-rm *.log
 rm *.dbg
 rm *.bak
-rm -r android_application/
 
 rm -r backup
 rm -r lib
 rm -r src/backup
 rm -r src/lib
-rm -r tests/profiler/backup
-rm -r tests/profiler/lib
 
 # Remove ugly Linux-related bug with NTFS filesystem
 
@@ -21,5 +17,5 @@ rm .fuse_hidden*
 
 # Remove logs
 
-rm log_*.txt
+rm *.log
 rm heap.trc
--- purge_all.sh
+++ purge_all.sh
@@ -4,16 +4,9 @@ bash purge.sh
 
 # Remove compiled files and screenshots
 
-rm pasdoc/html/*
-rm -r Release
-rm *.apk
-rm *.jpg
-rm *.scr.png
+
 
 # Compiled executables
 
-rm Decoherence_Desktop
-rm Decoherence_Desktop.exe
-rm Constructor_Tool
-rm Constructor_Tool.exe
-rm tests/profiler/ProfilerTest
+rm Decoherence
+rm Decoherence.exe
