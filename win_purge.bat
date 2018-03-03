d1a700df2686410380323e5ca89825b4c315bae5
eugeneloza <zhloza@gmail.com>
2018-03-03 21:54:06 +0200

    return windows scripts (package&purge)

---
+++ win_purge.bat
@@ -0,0 +1,11 @@
+del /q *.o
+del /q *.ppu
+del /q *.so
+del /q *.log
+del /q *.dbg
+del /q *.bak
+del /q log_*.txt
+rmdir /q /s backup
+rmdir /q /s lib
+del /q .fuse_hidden*
+del /q heap.trc
---
+++ win_purge_all.bat
@@ -0,0 +1,6 @@
+rmdir /q /s Release
+del /q decoherence
+del /q decoherence.exe
+del /q deco_*.png
+win_purge.bat
+
