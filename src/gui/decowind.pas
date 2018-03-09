b2265efde30bb6f19beca1b9bd85c6e0c3e1102a
eugeneloza <zhloza@gmail.com>
2018-03-09 11:48:55 +0200

    DWindImage.Update addresses https://github.com/eugeneloza/decoherence/issues/404

--- src/gui/decowind.pas
+++ src/gui/decowind.pas
@@ -89,8 +89,18 @@ var
 begin
   inherited Update;
   PhaseShift := DeltaT * PhaseSpeed;
-  Phase += PhaseShift * (1 + 0.1 * DRND.Random);
-
+  if PhaseShift < 0.5 then
+  begin
+    Phase += PhaseShift * (1 + 0.1 * DRND.Random);
+    if Phase > 1 then
+      Phase -= 1;
+    OpacityPhase += PhaseShift / 2 * (1 + 0.2 * DRND.Random);
+    if OpacityPhase > 1 then
+      OpacityPhase -= 1;
+  end
+  else
+    { if it was too long since last frame, reset the wind phase }
+    ResetPhase;
 end;
 
 {----------------------------------------------------------------------------}
