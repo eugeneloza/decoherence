0cb280493fc8759a67a113e48a8302f51b3e0c60
eugeneloza <zhloza@gmail.com>
2018-06-02 18:50:15 +0300

    update profiler scripts to work with new folder system addresses https://github.com/eugeneloza/decoherence/issues/507

--- profiler_off.sh
+++ profiler_off.sh
@@ -1,6 +1,4 @@
-cd src
-sed -i -- "s/doStartProfiler({\$IF DECLARED(ClassName)}ClassName+'.'+{\$ENDIF}{\$I %CURRENTROUTINE%});/{StartProfiler}/g" *.pas
-sed -i -- "s/doStartProfiler({\$IF DECLARED(ClassName)}ClassName+'.'+{\$ENDIF}{\$I %CURRENTROUTINE%});/{StartProfiler}/g" *.inc
-sed -i -- "s/doStopProfiler({\$IF DECLARED(ClassName)}ClassName+'.'+{\$ENDIF}{\$I %CURRENTROUTINE%});/{StopProfiler}/g" *.pas
-sed -i -- "s/doStopProfiler({\$IF DECLARED(ClassName)}ClassName+'.'+{\$ENDIF}{\$I %CURRENTROUTINE%});/{StopProfiler}/g" *.inc
-cd ..
+find src/ -type f -name '*.pas' -exec sed -i -- "s/doStartProfiler({\$IF DECLARED(ClassName)}ClassName+'.'+{\$ENDIF}{\$I %CURRENTROUTINE%});/{StartProfiler}/g" {} +
+find src/ -type f -name '*.inc' -exec sed -i -- "s/doStartProfiler({\$IF DECLARED(ClassName)}ClassName+'.'+{\$ENDIF}{\$I %CURRENTROUTINE%});/{StartProfiler}/g" {} +
+find src/ -type f -name '*.pas' -exec sed -i -- "s/doStopProfiler({\$IF DECLARED(ClassName)}ClassName+'.'+{\$ENDIF}{\$I %CURRENTROUTINE%});/{StopProfiler}/g" {} +
+find src/ -type f -name '*.inc' -exec sed -i -- "s/doStopProfiler({\$IF DECLARED(ClassName)}ClassName+'.'+{\$ENDIF}{\$I %CURRENTROUTINE%});/{StopProfiler}/g" {} +
--- profiler_on.sh
+++ profiler_on.sh
@@ -1,6 +1,4 @@
-cd src
-sed -i -- "s/{StartProfiler}/doStartProfiler({\$IF DECLARED(ClassName)}ClassName+'.'+{\$ENDIF}{\$I %CURRENTROUTINE%});/g" *.pas
-sed -i -- "s/{StartProfiler}/doStartProfiler({\$IF DECLARED(ClassName)}ClassName+'.'+{\$ENDIF}{\$I %CURRENTROUTINE%});/g" *.inc
-sed -i -- "s/{StopProfiler}/doStopProfiler({\$IF DECLARED(ClassName)}ClassName+'.'+{\$ENDIF}{\$I %CURRENTROUTINE%});/g" *.pas
-sed -i -- "s/{StopProfiler}/doStopProfiler({\$IF DECLARED(ClassName)}ClassName+'.'+{\$ENDIF}{\$I %CURRENTROUTINE%});/g" *.inc
-cd ..
+find src/ -type f -name '*.pas' -exec sed -i -- "s/{StartProfiler}/doStartProfiler({\$IF DECLARED(ClassName)}ClassName+'.'+{\$ENDIF}{\$I %CURRENTROUTINE%});/g" {} +
+find src/ -type f -name '*.inc' -exec sed -i -- "s/{StartProfiler}/doStartProfiler({\$IF DECLARED(ClassName)}ClassName+'.'+{\$ENDIF}{\$I %CURRENTROUTINE%});/g" {} +
+find src/ -type f -name '*.pas' -exec sed -i -- "s/{StopProfiler}/doStopProfiler({\$IF DECLARED(ClassName)}ClassName+'.'+{\$ENDIF}{\$I %CURRENTROUTINE%});/g" {} +
+find src/ -type f -name '*.inc' -exec sed -i -- "s/{StopProfiler}/doStopProfiler({\$IF DECLARED(ClassName)}ClassName+'.'+{\$ENDIF}{\$I %CURRENTROUTINE%});/g" {} +
